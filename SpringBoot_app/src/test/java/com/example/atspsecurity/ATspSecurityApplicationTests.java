0a88eef9940a076cdb454eb622d34136af3de0a4
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-05-08 16:02:41 +0200

    TSP-30 routing-cloud
    Dodanie uri dla mikroserwisów

---
+++ SpringBoot_app/src/test/java/com/example/atspsecurity/ATspSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atspsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATspSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
