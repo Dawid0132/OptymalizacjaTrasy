133696a844cbebf99eb01b18834f0978f69c4337
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-05-10 20:16:58 +0200

    TSP-41 Entity Repository

---
+++ SpringBoot_app/userAuthRest/src/main/java/com/example/userauthrest/DB/Entities/User.java
@@ -0,0 +1,89 @@
+package com.example.userauthrest.DB.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@NamedQuery(name = "User.findUserByEmail", query = "select u from User u where u.email=?1")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String firstname;
+
+    @Column
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String lastname;
+
+    @Column(unique = true)
+    @NotBlank
+    @Size(min = 5, max = 50)
+    @Email
+    private String email;
+
+    @Column
+    @NotBlank
+    @Size(max = 100)
+    @Pattern(regexp = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$")
+    private String password;
+
+    public User(Long id, String firstname, String lastname, String email, String password) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ SpringBoot_app/userAuthRest/src/main/java/com/example/userauthrest/UserAuthRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.userauthrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthRestApplication.class, args);
+    }
+
+}
