62b25a3723a1fc0c241aac6cd5bed9d44c149eba
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-05-11 07:11:04 +0200

    TSP-51 Walidacja dla ochrony endpointów

--- Python_app/Dashboard_api.py
+++ Python_app/Dashboard_api.py
@@ -1,6 +1,18 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
 
-dashboard_api = Blueprint('dashboard_api', __name__, template_folder='templates', url_prefix='/dashboard')
+dashboard_api = Blueprint('Dashboard_api', __name__, template_folder='templates', url_prefix='/dashboard')
+
+
+@dashboard_api.before_request
+def check_auth():
+    if request.endpoint in UNPROTECTED_ENDPOINTS:
+        if session.get('authenticated') is True:
+            return redirect(url_for('dashboard_api.index'))
+        return
+
+    if session.get('authenticated') is not True:
+        return redirect(url_for('user_api.login'))
+    return
 
 
 @dashboard_api.route("/")
