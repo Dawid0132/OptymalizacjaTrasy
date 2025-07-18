b44f8ae3e153bb01a65c833ba684fd647f329b41
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-07-18 10:22:16 +0200

    TSP-65 Stworzenie projektu databaseCore

---
+++ SpringBoot_app/databaseCore/src/main/java/com/example/databaseCore/DatabaseCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseCoreApplication.class, args);
+	}
+
+}
