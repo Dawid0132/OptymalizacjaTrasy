b0b26f4876de3f59ea8ac43444e7629eb61694ef
Dawid Janas <124461619+DawidJanas123@users.noreply.github.com>
2025-07-20 12:18:02 +0200

    TSP-67 Dodanie serwisu dla obsługi logiki dla User.

--- SpringBoot_app/serviceUserAuthRest/src/main/java/com/example/serviceuserauthrest/Controller/UserServiceController.java
+++ SpringBoot_app/serviceUserAuthRest/src/main/java/com/example/serviceuserauthrest/Controller/UserServiceController.java
@@ -1,12 +1,10 @@
 package com.example.serviceuserauthrest.Controller;
 
+import com.example.serviceuserauthrest.Pojo.Request.PasswordChanged;
 import com.example.serviceuserauthrest.Pojo.Request.UserRegister;
 import com.example.serviceuserauthrest.Service.ServiceUserAuthRestService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest/user/v1")
@@ -20,6 +18,26 @@ public class UserServiceController {
     @PostMapping(path = "/register")
     public ResponseEntity<String> register(@RequestBody UserRegister userRegister) {
         return ResponseEntity.ok(serviceUserAuthRestService.register(userRegister));
+    }
+
+    @GetMapping(path = "/logout/{user_id}")
+    public ResponseEntity<Void> logout(@PathVariable("user_id") Long user_id, @RequestHeader("Authorization") String authToken) {
+        return ResponseEntity.ok(serviceUserAuthRestService.logout(user_id, authToken));
+    }
+
+    @PostMapping(path = "/password/change/{user_id}")
+    public ResponseEntity<Void> password_change(@PathVariable("user_id") Long user_id, @RequestBody PasswordChanged passwordChanged, @RequestHeader("Authorization") String authToken) {
+        return ResponseEntity.ok(serviceUserAuthRestService.password_change(user_id, passwordChanged, authToken));
+    }
+
+    @PostMapping(path = "/password/verify/{user_id}")
+    public ResponseEntity<Void> password_verify(@PathVariable("user_id") Long user_id, @RequestBody PasswordChanged passwordChanged, @RequestHeader("Authorization") String authToken) {
+        return ResponseEntity.ok(serviceUserAuthRestService.password_verify(user_id, passwordChanged, authToken));
+    }
+
+    @GetMapping(path = "/account/delete/{user_id}")
+    public ResponseEntity<Void> account_delete(@PathVariable("user_id") Long user_id, @RequestHeader("Authorization") String authToken) {
+        return ResponseEntity.ok(serviceUserAuthRestService.account_delete(user_id, authToken));
     }
 
 }
