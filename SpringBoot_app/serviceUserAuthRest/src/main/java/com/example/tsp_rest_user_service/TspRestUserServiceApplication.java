0f8eacc601721f53bb097df21aa88fb219b77d28
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-08-20 13:36:07 +0200

    serwis

---
+++ SpringBoot_app/serviceUserAuthRest/src/main/java/com/example/tsp_rest_user_service/TspRestUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsp_rest_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspRestUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TspRestUserServiceApplication.class, args);
+    }
+
+}
---
+++ SpringBoot_app/serviceUserAuthRest/src/test/java/com/example/tsp_rest_user_service/TspRestUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsp_rest_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspRestUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
