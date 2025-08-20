e15869b647bd0957ff958880d099e06021aff8e1
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2025-08-20 13:14:29 +0200

    Delete SpringBoot_app/databaseCore/src directory

--- SpringBoot_app/databaseCore/src/main/java/com/example/databaseCore/Repositories/Maps/CoordinatesForTripsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.databaseCore.Repositories.Maps;
-
-
-import com.example.databaseCore.Entities.Maps.CoordinatesForTrips;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoordinatesForTripsRepository extends CrudRepository<CoordinatesForTrips, Long> {
-}
